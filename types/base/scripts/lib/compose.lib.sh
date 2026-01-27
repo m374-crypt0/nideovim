2a2b20330f3c3f597e5fd6a943c91c61cbcf01a1
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 15:50:12 +0000

    fix: types
    custom override compose fixed for all types, in particular for those
    that have ancestors

--- types/base/scripts/lib/compose.lib.sh
+++ types/base/scripts/lib/compose.lib.sh
@@ -17,6 +17,20 @@ export_variables_for_compose() {
     export_user_info
 }
 
+touch_external_custom_compose_override_files_for_all_ancestors() {
+  local type_dir &&
+    type_dir="$(pwd)"
+
+  while [ "$(basename "$type_dir")" = 'ancestor' ]; do
+    touch "$type_dir/docker/custom_external_override.yaml"
+    cd "$type_dir"/.. || return $?
+
+    type_dir=$(pwd)
+
+    cd - >/dev/null || return $?
+  done
+}
+
 reset_external_custom_compose_override_file() {
   local compose_dir &&
     compose_dir="$(get_type_dir_from_current_ancestor_dir)/docker"
@@ -60,7 +74,8 @@ sync_external_custom_override_compose_file_into_instance_docker_directory() {
 }
 
 apply_external_custom_compose_override() {
-  reset_external_custom_compose_override_file &&
+  touch_external_custom_compose_override_files_for_all_ancestors &&
+    reset_external_custom_compose_override_file &&
     check_external_custom_compose_override_file &&
     sync_external_custom_override_compose_file_into_instance_docker_directory
 }
