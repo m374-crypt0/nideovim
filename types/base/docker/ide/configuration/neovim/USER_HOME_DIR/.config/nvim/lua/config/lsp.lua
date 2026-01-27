58702a8fd2268fb20de1128a5bb8bd268577b13c
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 15:50:12 +0000

    feat: json file type handling
    biome as lsp and linter and formatter

--- types/base/docker/ide/configuration/neovim/USER_HOME_DIR/.config/nvim/lua/config/lsp.lua
+++ types/base/docker/ide/configuration/neovim/USER_HOME_DIR/.config/nvim/lua/config/lsp.lua
@@ -1,23 +1,12 @@
 require("mason-lspconfig").setup()
 require("lspconfig").clangd.setup({})
-
-require("lspconfig").denols.setup({
-	init_options = {
-		lint = true,
-		unstable = true,
-	},
-	root_dir = require("lspconfig").util.root_pattern(
-		"package.json",
-		"deno.json",
-		"deno.jsonc",
-		".deno.json",
-		".deno.jsonc"
-	),
-})
+require("lspconfig").biome.setup({})
 
 require("conform").setup({
 	formatters_by_ft = {
 		solidity = { "solidity_forge" },
+		json = { "biome" },
+		jsonc = { "biome" },
 	},
 	formatters = {
 		solidity_forge = {
